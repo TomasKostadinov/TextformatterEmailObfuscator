85c1ed2636f94acfecd176dc38a87aeda26ff38a
Tomas Kostadinov <TomasKostadinov@users.noreply.github.com>
2020-02-10 11:22:39 +0100

    [bugfix] emails not opening

--- TextformatterEmailObfuscator/templates/encrypt-js.inc.php
+++ TextformatterEmailObfuscator/templates/encrypt-js.inc.php
@@ -1,6 +1,6 @@
 <script type="application/javascript">
   function decryptAndOpen(key) {
-    window.location.href = key.split("").reverse().join("").replace("@@", "@");
+    window.location.href = "mailto:" + key.split("").reverse().join("").replace("@@", "@");
   }
   
   function decryptAndReturn(key) {
